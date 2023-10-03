fbd61ef6372b4cbdbdedefd4711cacf4a83114ed
A_Creature <a_creature@mail.ru>
2023-10-03 20:37:07 +0300

    Lecture 1 C# Console example 3

---
+++ lecture_1_cs_3/Program.cs
@@ -0,0 +1,9 @@
+﻿int numA = 3;
+int numB = 5;
+
+//Console.Write("Ваше имя: ");
+//string username = Console.ReadLine();
+//Console.Write("Привет, ");
+int res = numA + numB;
+Console.WriteLine(res);
+// 
